ba79fd7ab3bab52a7a071b8f628cc55607d296a6
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-01-08 20:17:20 +0530

    stage - 0 by Aditya

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/footer"
+import { Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+<Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    
+
+   
+
+
+</Routes>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
